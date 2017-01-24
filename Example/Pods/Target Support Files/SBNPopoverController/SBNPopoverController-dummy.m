9e94c4452133b603681726c28dc04eabd6b08e18
黄安华 <765748448@qq.com>
2017-01-24 17:37:14 +0800

    initial

---
+++ Example/Pods/Target Support Files/SBNPopoverController/SBNPopoverController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SBNPopoverController : NSObject
+@end
+@implementation PodsDummy_SBNPopoverController
+@end
---
+++ Example/Pods/Target Support Files/SBNPopoverController/SBNPopoverController-umbrella.h
@@ -0,0 +1,12 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "SBNPopoverController.h"
+#import "SBNPopoverOverlayView.h"
+#import "SBNPopoverTheme.h"
+#import "UIView+Extension.h"
+
+FOUNDATION_EXPORT double SBNPopoverControllerVersionNumber;
+FOUNDATION_EXPORT const unsigned char SBNPopoverControllerVersionString[];
+
